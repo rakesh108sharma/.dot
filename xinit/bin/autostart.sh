d98de23a1ccc797da2d4fa96783dba7abca751ec
rakesh108sharma <rakesh108sharma@gmail.com>
2022-04-30 09:09:07 +0200

    cleaned autostart

--- xinit/bin/autostart.sh
+++ xinit/bin/autostart.sh
@@ -4,7 +4,6 @@
 # do all "wanted" things at system start
 
 #xrandr --output HDMI-A-0 --mode 1280x720 &
-#sudo ntpd -s &
 setxkbmap de
 
 xset -dpms # standby - suspend -  off
@@ -15,10 +14,8 @@ pavucontrol-qt &
 picom -b &
 
 st -e /bin/zsh &
-#run darkhttpd ~/wiki/my_wiki/site/ --addr 192.168.1.22 --daemon
 
 sleep 2
-#chromium http://192.168.1.22:8080 &
 chromium &
 st -e system.sh &
 
@@ -27,21 +24,19 @@ st -e system.sh &
 wallpapershow.sh &
 
 # hides the mouse when writing
-#xbanish &
+xbanish &
 
 tldr --update &
 xbps-query -Rs '' >"$HOME/voidpackages" &
-via -ro &
+#via -ro &
 
 devmon &
 
 sleep 1
 amixer set Master 20%
 mimic -t "All systems online"
-#jupyter-notebook &
 clipit &
-emacs --daemon
+#emacs --daemon
 xrdb -load .Xresources 
-#sudo killall ntpd
 
 doas /etc/zzz.d/resume/update &
