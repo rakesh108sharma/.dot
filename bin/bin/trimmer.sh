8c50d6ced61c9614ef33298c81b638b4f708fd22
rakesh108sharma <rakesh108sharma@gmail.com>
2023-06-01 22:01:50 +0200

    M minor edits

--- bin/bin/trimmer.sh
+++ bin/bin/trimmer.sh
@@ -12,7 +12,7 @@ herber "Trim         OR not to Trim?!" "(left-mouse) OR (right-mouse)" && exit 1
 doas fstrim / || herber "cannot trim   /"
 doas fstrim /home || herber "cannot trim   /home"
 doas fstrim /media/SSD-120G || herber "cannot trim   /media/SSD-120G"
-
+doas fstrim /media/SSD-240G || herber "cannot trim   /media/SSD-240G"
 herbe "trimming complete!"
 
 exit 0
